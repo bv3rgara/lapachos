7ab9a54b396225fa20a8823ff1b9e86078098d52
Bruno <vergara_bruno@outlook.com>
2019-09-09 19:54:39 -0300

    borrado de js innecesarios en el index

--- index.php
+++ index.php
@@ -323,9 +323,6 @@
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- Plugin JavaScript -->
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <!-- Contact form JavaScript -->
-    <script src="js/jqBootstrapValidation.js"></script>
-    <script src="js/contact_me.js"></script>
     <!-- Custom scripts for this template -->
     <script src="js/agency.min.js"></script>
   </body>
