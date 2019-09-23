7de10fac38e163817099c366f8aecef09175ad3d
bv3rgara <bruno.vergara@aguasdecorrientes.com>
2019-09-23 15:02:43 -0300

    reubicar logo en moviles en la esquina izquierda

--- index.php
+++ index.php
@@ -31,12 +31,12 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
       <div class="container">
-        <img class="img-responsive d-block mx-auto" src="img/logo.png" alt="">
+        <img class="img-responsive d-block ml" src="img/logo.png" alt="">
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fas fa-bars"></i>
         </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
+        <div class="collapse navbar-collapse mr" id="navbarResponsive">
           <ul class="navbar-nav text-uppercase ml-auto">
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="#services"><strong>Servicios</strong></a>
