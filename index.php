7b0ee7b27809a46c148cb4292f9672b0aac10d43
bv3rgara <bruno.vergara@aguasdecorrientes.com>
2019-09-26 07:59:57 -0300

    agregando Google Analytics

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html lang="es">
   <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-148795022-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-148795022-1');
+    </script>
     <meta charset="utf-8">
     <link rel="shortcut icon" href="img/favicon.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
